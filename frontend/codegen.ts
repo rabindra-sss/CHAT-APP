84462f6afea4257d64c04cc403e94d0eba72b765
RABINDRANATH DAS <rdas721433@gmail.com>
2024-08-06 23:53:22 +0530

    prisma migraton

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from "@graphql-codegen/cli"
 
 const config: CodegenConfig = {
-  schema: "http://localhost:3000/graphql",
+  schema: "http://localhost:3030/graphql",
   documents: ["src/graphql/**/*.ts"],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
